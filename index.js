7be891497e927d638d16d07f7e4930bead9bbdf9
Marco Amador <marcoamador214@gmail.com>
2022-10-12 15:40:34 -0700

    commit tree 5ef0745855af4df296a90fe72c9c58e7b2fc8ce1

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+    return Math.abs(pickup - 42);
+}
+
+function distanceFromHqInFeet(pickup) {
+   return distanceFromHqInBlocks(pickup) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((destination - start) * 264);
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+
+    if (distance <= 400) {
+        return 0;
+    }
+
+    if (distance > 2500) {
+        return "cannot travel that far";
+    }
+
+    if (distance > 2000) {
+        return 25;
+    }
+
+    else {
+        return ((distance-400) * 0.02);
+    }
+
+}
